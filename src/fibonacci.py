f8bd8946d113400ffe19fd84cd429ab560bc83a3
Janis Pirktl <janis.pirktl@students.ffhs.ch>
2024-02-07 20:06:35 +0100

    added comments

--- src/fibonacci.py
+++ src/fibonacci.py
@@ -1,9 +1,12 @@
 import time
 import math
 
+#wird benutzt, um die 30 ersten Fibonacci-Zahlen zu berechnen
 number_of_calls = 31
 
 
+
+#rekursive Funktion um die n-te Fibonacci-Zahl zu berechnen
 def fib(n):
     if n <= 1:
         return n
@@ -11,6 +14,7 @@ def fib(n):
         return fib(n - 1) + fib(n - 2)
 
 
+#Funktion um die Funktionsaufrufe der rekursiven Funktion zu zählen
 def fib_calls(n, counter):
     counter[0] += 1 #zählt den aktuellen Aufruf
     if n <= 1:
@@ -19,6 +23,24 @@ def fib_calls(n, counter):
         return fib_calls(n - 1, counter) + fib_calls(n - 2, counter)
 
 
+#Funktion um die n-te Fibonacci-Zahl dynamisch zu berechnen
+def fib_dynamic(n):
+    if n <= 1:
+        return n
+    fib_numbers = [0, 1] + [0] * (n - 1)  # Initialisiert ein Array mit den ersten zwei Fibonacci-Zahlen und setzt den Rest auf 0
+    for i in range(2, n + 1):
+        fib_numbers[i] = fib_numbers[i - 1] + fib_numbers[i - 2]
+    return fib_numbers[n]
+
+
+#Funktion um die n-te Fibonacci-Zahl mit Binets Formel zu berechnen
+def fib_binet(n):
+    golden_ratio = (1 + math.sqrt(5)) / 2
+    return round((golden_ratio ** n - (1 - golden_ratio) ** n) / math.sqrt(5))
+
+
+#Berechnet die ersten 30 Fibonacci-Zahlen mit der rekursiven Funktion
+#Gibt zusätzlich die jeweilige Anzahl Aufrufe der rekursiven Funktion und die benötigte Zeit aus
 print("Mittels rekursiver Funktion:")
 for i in range(number_of_calls):
     counter = fib_calls(i, [0])[1][0]
@@ -35,15 +57,8 @@ print()
 
 
 
-def fib_dynamic(n):
-    if n <= 1:
-        return n
-    fib_numbers = [0, 1] + [0] * (
-            n - 1)  # Initialisiert ein Array mit den ersten zwei Fibonacci-Zahlen und setzt den Rest auf 0
-    for i in range(2, n + 1):
-        fib_numbers[i] = fib_numbers[i - 1] + fib_numbers[i - 2]
-    return fib_numbers[n]
-
+#Berechnet die ersten 30 Fibonacci-Zahlen mit der dynamischen Funktion
+#Gibt zusätzlich die benötigte Zeit aus
 total_time_dynamic = 0
 print("Mittels dynamischer Programmierung")
 for i in range(number_of_calls):
@@ -55,7 +70,7 @@ for i in range(number_of_calls):
     print(
         f"Die {i}.te Fibonacci-Zahl ist: {fib(i)}; Dafür wurden {duration} Nanosekunden gebraucht")
 
-
+#Berechnet die durschnittlich benötigte Zeit und gibt sie aus
 average_time_dynamic = total_time_dynamic / number_of_calls
 print(f"Durchschnittliche Zeit der Berechnung: {round(average_time_dynamic)} Nanosekunden")
 print()
@@ -63,10 +78,9 @@ print()
 
 
 
-def fib_binet(n):
-    golden_ratio = (1 + math.sqrt(5)) / 2
-    return round((golden_ratio ** n - (1 - golden_ratio) ** n) / math.sqrt(5))
 
+#Berechnet die ersten 30 Fibonacci-Zahlen mit der Formel von Binet
+#Gibt zusätzlich die benötigte Zeit aus
 total_time_binet = 0
 print("Mittels Binets Formel")
 for i in range(number_of_calls):
@@ -77,10 +91,15 @@ for i in range(number_of_calls):
     total_time_binet = total_time_binet + duration
     print(
         f"Die {i}.te Fibonacci-Zahl ist: {fib(i)}; Dafür wurden {duration} Nanosekunden gebraucht")
+
+#Berechnet die durchschnittlich benötigte Zeit und gibt sie aus
 average_time_binet = total_time_binet / number_of_calls
 print(f"Durchschnittliche Zeit der Berechnung: {round(average_time_binet)} Nanosekunden")
+print()
 
 
+#Berechnet 200 mal die ersten 30 Fibonacci-Zahlen mit der dynamischen Formel und Binets Formel
+#Vergleicht die dabei durchschnittlich benötigte Zeit
 binet_was_faster = 0
 dynamic_was_faster = 0
 for i in range(200):
