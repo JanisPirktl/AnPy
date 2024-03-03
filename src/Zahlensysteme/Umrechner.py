3af2de3dfbd3f2f4db318913c6362a1478af6e4e
Janis Pirktl <janis.pirktl@students.ffhs.ch>
2024-03-03 19:43:07 +0100

    Done

---
+++ src/Zahlensysteme/Umrechner.py
@@ -0,0 +1,19 @@
+def umrechnen(zahl, eingabe_basis, ziel_basis):
+
+    dezimalzahl = int(str(zahl), eingabe_basis)
+
+    if ziel_basis == 10:
+        print(f"{dezimalzahl} (Basis {ziel_basis})")
+    else:
+        alphabet = "0123456789ABCDEF"
+        umgerechnete_zahl = ""
+        while dezimalzahl > 0:
+            umgerechnete_zahl = alphabet[dezimalzahl % ziel_basis] + umgerechnete_zahl
+            dezimalzahl //= ziel_basis
+        print(f"{umgerechnete_zahl} (Basis {ziel_basis})")
+
+
+umrechnen(12345, 16, 10)
+
+
+
