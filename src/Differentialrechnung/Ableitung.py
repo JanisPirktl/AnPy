6a495b690df8bb8a47e2d5691234a3f59156870c
JanisPirktl <142509118+JanisPirktl@users.noreply.github.com>
2024-04-12 22:24:45 +0200

    Done

--- src/Differentialrechnung/Ableitung.py
+++ src/Differentialrechnung/Ableitung.py
@@ -2,7 +2,8 @@ from sympy import symbols, diff
 
 # Definieren der Variablen und der Funktion
 x = symbols('x')
-funktion = 3 * x**5
+y = symbols('y')
+funktion = (x**4 + 5)**7
 
 # Berechnen der ersten Differentialrechnung der Funktion
 ableitung = diff(funktion, x)
